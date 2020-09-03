fd9aa99b93bae657b5444644974fdb64e6267085
shenchunhang <ckdxc@qq.com>
2020-09-03 17:13:11 +0800

    feat(proxy):

---
+++ src/main/java/com/shenchunhang/designpattern/proxy/Image.java
@@ -0,0 +1,5 @@
+package com.shenchunhang.designpattern.proxy;
+
+public interface Image {
+    void display();
+}
---
+++ src/main/java/com/shenchunhang/designpattern/proxy/ProxyImage.java
@@ -0,0 +1,18 @@
+package com.shenchunhang.designpattern.proxy;
+
+public class ProxyImage implements Image {
+    private RealImage realImage;
+    private String fileName;
+
+    public ProxyImage(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void display() {
+        if (null == realImage) {
+            realImage = new RealImage(fileName);
+        }
+        realImage.display();
+    }
+}
